fda1f8cbbd681c1d1d77a3f7381972540191cd7d
candicoded <103226511+candicoded@users.noreply.github.com>
2023-11-10 10:52:23 -0500

    fixed spacing

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const shareButton = document.querySelector('.share-icon-bg');
 const shareSection = document.querySelector('.share');
+
+console.log(shareButton);
+
+// shareButton.addEventListener('click', function test(){
+//   console.log('clicked!');
+// });
