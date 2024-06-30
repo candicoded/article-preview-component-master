9885c9e602da11b383e732900fc5ec5a7d91c60f
candicoded <candiator@gmail.com>
2024-06-30 12:49:37 -0400

    finished javascript

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const shareBtn = document.querySelector('.share-icon-light');
+const shareBtnLight = document.querySelector('.share-icon-light');
+const shareBtnDark = document.querySelector('.share-icon-dark');
 const shareSection = document.querySelector('.share-section');
+const authorSection = document.querySelector('.author-section');
 
-shareBtn.addEventListener('click', () => {
-  console.log('clicked')
-  // shareSection.classList.toggle("hidden")
+shareBtnLight.addEventListener('click', () => {
+  shareSection.classList.toggle("hidden")
+  authorSection.classList.toggle("hidden")
+})
+
+shareBtnDark.addEventListener('click', () => {
+  authorSection.classList.toggle("hidden")
+  shareSection.classList.toggle("hidden")
 })
